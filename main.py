db0518833ca6250d7fcbc3633febd235b189dd38
Hbear10 <140596511+Hbear10@users.noreply.github.com>
2024-06-05 18:54:49 +0100

    Update main.py
    added coyote time and other QoL

--- main.py
+++ main.py
@@ -5,41 +5,45 @@ pygame.init()
 BGC = (255, 255, 255)
 screen = pygame.display.set_mode((1280, 960))
 
-map = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
-       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "x"]]
+world_map = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
+             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "x"]]
 
 running = True
-gravity = 2
+gravity_and_jumpspeed = 2
 player_x = 64
 player_y = 640
 direction = "r"
 
 speed = 2
-jumpspeed = 2
 
 state = "idle"
 
 start_jump = 0
 
-img = pygame.image.load("assets\idle1_right.png")
+img = pygame.image.load("assets\\idle1_right.png")
 
 # sprite stages
 idle_sprite = 1
 left_sprite = 1
 right_sprite = 1
+
+#length of cyote(frames)
+cyote_time = 15
+##update cyote
+cyote = cyote_time
 
 
 def get_sprite():
@@ -120,7 +124,7 @@ def draw_screen():
     screen.blit(img, (player_x, player_y))
 
     y_count = 0
-    for y in map:
+    for y in world_map:
         x_count = 0
         for x in y:
             if x == 1:
@@ -130,9 +134,6 @@ def draw_screen():
 
 
 player = pygame.draw.rect(screen, (255, 0, 0), (player_x, player_y, 64, 96))
-
-past_x = player_x
-past_y = player_y
 
 while running:
     for event in pygame.event.get():
@@ -153,51 +154,52 @@ while running:
         if state != "fall" and state != "jump_up":
             state = "idle"
     elif key[pygame.K_LEFT]:
+        direction = "l"
         if player_x > 0 and not (
-                map[(player_y + 1) // 64][player_x // 64] == 1 or map[(player_y + 95) // 64][player_x // 64] == 1 or
-                map[(player_y + 48) // 64][player_x // 64] == 1):
+                world_map[(player_y + 1) // 64][player_x // 64] == 1 or world_map[(player_y + 95) // 64][player_x // 64] == 1 or
+                world_map[(player_y + 48) // 64][player_x // 64] == 1):
             player_x -= speed
         if state != "fall" and state != "jump_up":
             state = "run"
-            direction = "l"
+
     elif key[pygame.K_RIGHT]:
-        if player_x <= 1216 and not (map[(player_y + 1) // 64][player_x // 64 + 1] == 1 or map[(player_y + 95) // 64][player_x // 64 + 1] == 1 or map[(player_y + 48) // 64][player_x // 64 + 1] == 1):
+        direction = "r"
+        if player_x <= 1216 and not (world_map[(player_y + 1) // 64][player_x // 64 + 1] == 1 or world_map[(player_y + 95) // 64][player_x // 64 + 1] == 1 or world_map[(player_y + 48) // 64][player_x // 64 + 1] == 1):
             player_x += speed
         if state != "fall" and state != "jump_up":
             state = "run"
-            direction = "r"
+
     else:
         if state != "fall" and state != "jump_up":
             state = "idle"
 
-    # checks if there is anything bellow player to check if you should fall
     if state == "idle" or state == "run":
-        if player_y > past_y:
-            state = "fall"
-        if map[(player_y + 97) // 64][player_x // 64] == 0 and map[(player_y + 97) // 64][player_x // 64 + 1] == 0:
-            state = "fall"
+        # checks if there is anything bellow player to check if you should fall and allows for cyote time
+        if world_map[(player_y + 97) // 64][player_x // 64] == 0 and world_map[(player_y + 97) // 64][player_x // 64 + 1] == 0:
+            if cyote == 0:
+                state = "fall"
+            else:
+                cyote -= 1
+        else:
+            cyote = cyote_time
     elif state == "jump_up":
         if player_y <= start_jump - 256:
             state = "fall"
         else:
             # set jump height
-            player_y -= jumpspeed
+            player_y -= gravity_and_jumpspeed
     elif state == "fall":
         # bottom collision and gravity
-        if map[(player_y + 96) // 64][(player_x + 1) // 64] == 1 or map[(player_y + 96) // 64][(player_x + 63) // 64] == 1:
+        if world_map[(player_y + 96) // 64][(player_x + 1) // 64] == 1 or world_map[(player_y + 96) // 64][(player_x + 63) // 64] == 1:
             state = "idle"
         else:
-            player_y += gravity
+            player_y += gravity_and_jumpspeed
 
     # top
-    if map[player_y // 64][(player_x + 1) // 64] == 1 or map[player_y // 64][(player_x + 63) // 64] == 1:
+    if world_map[player_y // 64][(player_x + 1) // 64] == 1 or world_map[player_y // 64][(player_x + 63) // 64] == 1:
         state = "fall"
-    # left
 
-    past_x = player_x
-    past_y = player_y
-
-    print(state)
+    #print(state)
 
     draw_screen()
     pygame.display.flip()
